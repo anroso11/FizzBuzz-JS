236f45a76697204115635e11fda8238a6bfedbf6
anroso11 <andresroberto1187@gmail.com>
2022-05-20 13:03:28 -0500

    Version 2 por el metodo for(iteradores) fizzbuzz

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-let i = 1;
+for(let i = 1; i < 100; i++){
 
-do{
-    if(i % 3 === 0 && i % 5 === 0){
-        document.write(`FIZZBUZZ:  ${i} </br>`);
-    }else if(i % 3 === 0){
-        document.write(`Fizz:  ${i} </br>`);
-    }else if(i % 5 === 0){
+     if(i % 3 === 0 && i % 5 ===0  ) 
+         document.write(`FIZZ BUZZ:  ${i} </br>`);
+     else if(i % 3 === 0){
+         document.write(`Fizz:  ${i} </br>`);
+     }else if(i % 5 ===0 ){
         document.write(`Buzz:  ${i} </br>`);
-    } 
-    i++;
-
-}while(i<=100)
+     }
+}
